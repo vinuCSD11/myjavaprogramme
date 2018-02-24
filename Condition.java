6d8ebe71ca5708980786734836bb7355e31c8bfc
vinuCSD11 <36789183+vinuCSD11@users.noreply.github.com>
2018-02-24 16:23:26 +0530

    new1

---
+++ Condition.java
@@ -0,0 +1,34 @@
+class Condition{
+	public static void main(String args[]){
+		int X = 10;
+		if ( X == 10 ){
+			System.out.println("value of X iS 10");
+		}else{
+			System.out.println("value of X iS not 10");
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[{
